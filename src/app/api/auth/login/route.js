73b443de15e9ca576602a3a012f10afa68bf44cc
anshika0010 <anshikabhadoria88@gmail.com>
2026-09-07 20:25:41 +0530

    url

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,38 +1,39 @@
-  import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-  export async function POST(req) {
+export async function POST(req) {
+  try {
+    const body = await req.json();
 
-    try {
+    const baseUrl = process.env.BACKEND_URL || process.env.NEXT_PUBLIC_BASE_URL;
+    console.log("BASE_URL:", JSON.stringify(baseUrl));
 
-      const body = await req.json();
-
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/login`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body),
-        }
-      );
-
-      const data = await response.json();
-
-      return NextResponse.json(data, {
-        status: response.status,
-      });
-    } catch (error) {
-      console.error("Login Route Error:", error);
-
-      return NextResponse.json(
-        {
-          success: false,
-          message: "Internal Server Error",
+    const response = await fetch(
+      `${baseUrl}/api/auth/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          status: 500,
-        }
-      );
-    }
+        body: JSON.stringify(body),
+      }
+    );
+
+    const data = await response.json();
+
+    return NextResponse.json(data, {
+      status: response.status,
+    });
+  } catch (error) {
+    console.error("Login Route Error:", error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      }
+    );
   }
+}
